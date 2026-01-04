62752bde0f4b34e38874f446afe41b880c495221
Amiya167 <neuvillette-fontaine@outlook.com>
2026-01-04 21:02:22 +0800

    change Web Famework to Vue

--- control-plane/Muelsyse-Web-UI/react-router.config.ts
+++
@@ -1,7 +0,0 @@
-import type { Config } from "@react-router/dev/config";
-
-export default {
-  // Config options...
-  // Disable SSR for liquid-glass-react compatibility (uses WebGL)
-  ssr: false,
-} satisfies Config;
